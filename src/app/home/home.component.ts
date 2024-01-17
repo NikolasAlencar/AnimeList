133ecfb7adef900c7d9302a2a06b407823a6d44f
NikolasAlencar <nikolasalencarrs73@gmail.com>
2024-01-17 19:53:39 -0300

    feat: implementa skeleton e ajusta scroll

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import {
   Component,
-  ElementRef,
   HostListener,
   OnInit,
-  ViewChild,
   afterNextRender,
   inject,
+  signal,
 } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { AnimesService } from '../services/animes.service';
@@ -13,14 +12,23 @@ import { JsonPipe, NgStyle } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { ajustaGrid } from '../../assets/util/AjustaGrid';
-import { LoaderComponent } from "../components/loader/loader.component";
+import { LoaderComponent } from '../components/loader/loader.component';
+import { SkeletonComponent } from '../components/skeleton/skeleton.component';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss',
-    imports: [MatCardModule, JsonPipe, NgStyle, MatIconModule, MatGridListModule, LoaderComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  imports: [
+    MatCardModule,
+    JsonPipe,
+    NgStyle,
+    MatIconModule,
+    MatGridListModule,
+    LoaderComponent,
+    SkeletonComponent
+  ],
 })
 export class HomeComponent implements OnInit {
   constructor() {
@@ -30,9 +38,8 @@ export class HomeComponent implements OnInit {
   animesService = inject(AnimesService);
   animes = this.animesService.getAnimes();
 
-  screenWidth = 100;
-  innerWidth!: number;
-  @ViewChild('content') contentRef!: ElementRef;
+  screenWidth = signal(0);
+  innerWidth = signal(0);
 
   ngOnInit(): void {
     this.animesService.getAnimesByPage(0);
@@ -40,7 +47,7 @@ export class HomeComponent implements OnInit {
 
   @HostListener('window:resize', ['$event'])
   onWindowResize() {
-    this.screenWidth = window.innerWidth / 9;
-    this.innerWidth = ajustaGrid(window.innerWidth);
+    this.screenWidth.set(window.innerWidth / 9);
+    this.innerWidth.set(ajustaGrid(window.innerWidth));
   }
 }
