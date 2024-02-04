9044ad1aea645400e63933a751488536abfa3f7e
NikolasAlencar <nikolasalencarrs73@gmail.com>
2024-02-04 14:55:05 -0300

    feat: adiciona global error handler

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, ErrorHandler } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { GlobalErrorHandler } from '../global-error-handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +13,9 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideAnimations(),
     provideHttpClient(withFetch()),
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler
+    }
   ],
 };
